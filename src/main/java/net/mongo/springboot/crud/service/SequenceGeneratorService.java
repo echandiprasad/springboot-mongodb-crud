3e3bdd9f268d3ecc4b7fc8f777a8bb515acbec1c
echandiprasad <echandiprasad@gmail.com>
2020-11-09 08:33:57 +0530

    removed hardcoded

--- src/main/java/net/mongo/springboot/crud/service/SequenceGeneratorService.java
+++ src/main/java/net/mongo/springboot/crud/service/SequenceGeneratorService.java
@@ -16,21 +16,18 @@ import net.mongo.springboot.crud.model.DatabaseSequence;
 @Service
 public class SequenceGeneratorService {
 
-    private MongoOperations mongoOperations;
+	private MongoOperations mongoOperations;
 
-    @Autowired
-    public SequenceGeneratorService(MongoOperations mongoOperations) {
-        this.mongoOperations = mongoOperations;
-    }
+	@Autowired
+	public SequenceGeneratorService(MongoOperations mongoOperations) {
+		this.mongoOperations = mongoOperations;
+	}
 
-    public long generateSequence(String seqName) {
+	public long generateSequence(String seqName) {
 
-        DatabaseSequence counter = mongoOperations.findAndModify(query(where("_id").is(seqName)),
-                new Update().inc("seq",1), options().returnNew(true).upsert(true),
-                DatabaseSequence.class);
-        return !Objects.isNull(counter) ? counter.getSeq() : 1;
+		DatabaseSequence counter = mongoOperations.findAndModify(query(where("_id").is(seqName)),
+				new Update().inc("seq", 1), options().returnNew(true).upsert(true), DatabaseSequence.class);
+		return !Objects.isNull(counter) ? counter.getSeq() : 1;
 
-        
-        select max(id) from employee;
-    }
+	}
 }
